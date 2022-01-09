a1c6aac846d75ec1c176bdcdbdce226515f8261a
pika <48164825+bleovy0820@users.noreply.github.com>
2022-01-09 11:06:50 -0500

    Minecraft server creator script

---
+++ fun/minecraft/createserver/createserver.sh
@@ -0,0 +1,24 @@
+echo Welcome to server creator! This script will help you create your very own minecraft server!
+echo Creating server in directory $(pwd), if you do not wish for you server to be here, press Control + C in the next 5 seconds.
+sleep 5
+echo Starting...
+echo Enter the name for your server. This is only visible to you. Make sure to use a unique name that isn\'t used by any other files in this folder.
+read dirname
+mkdir $dirname
+cd $dirname
+echo Downloading jar for 1.18...
+echo If you get an error, please install wget
+curl https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar > server.jar
+echo Please ensure that java 17 or later is installed, check your java version with java --version
+echo Please wait...
+java -jar server.jar --initSettings
+echo Creating start script...
+echo How much ram should your server be able to use? Depending on how much ram your computer has, you might want to use 2, 4, 8, or even 16 but you should leave enough ram for your system to run.
+read wam
+echo "cd $(pwd); exec java -Xmx"$wam"G -Xms"$wam"G -jar server.jar" > start.sh
+chmod +x start.sh
+echo Complete!
+echo Open eula.txt and change false to true.
+echo "If you don't want the gui to show up when starting your server, open start.sh and add nogui to the end."
+echo Start your server by running start.sh the same way you ran this script.
+echo To make changes to or start your server, make sure to type cd "$dirname" before doing anything.
