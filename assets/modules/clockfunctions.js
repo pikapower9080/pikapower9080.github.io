590fffb478f6e5e0565b327f393a973c36f6d903
pikapower9080 <pikapower9080@gmail.com>
2022-02-09 12:12:59 +0000

    Hotfix: The time on the page's title not formatted

--- assets/modules/clockfunctions.js
+++ assets/modules/clockfunctions.js
@@ -144,6 +144,16 @@ function getseconds() {
     }
     return strTime;
   }
+  function formatTitle(){
+    const date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes
+    return strTime;
+  }
 
 function getElement(id) {
   return document.getElementById(id)
